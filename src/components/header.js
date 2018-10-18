ae635929f5af94711c880ae9a38168c7f5d65483
Joseph Jennings III <jajenningsiii@gmail.com>
2018-10-17 23:36:22 -0700

    routes and pages working properly

--- src/components/header.js
+++ src/components/header.js
@@ -1,41 +1,49 @@
 import React, { Component } from 'react';
 import '../css/header-css.css';
-import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Link, NavLink, Switch, Route } from 'react-router-dom';
+import About from '../pages/about.js';
+import Contact from '../pages/contact.js';
+import Resume from '../pages/resume.js';
 
 class Header extends Component {
     render() {
         return (
-            <header>
-                <section className="title">
-                    Joseph Jennings III
-                </section>
-                <Router>
-                    <section className="menu">
-                        <ul>
-                            <li>
-                                <NavLink to ='/' exact activeStyle={
-                                    {borderTop: 'solid 3px #000'}
-                                }>Projects</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to ='/pages/about' exact activeStyle={
-                                    {borderTop: 'solid 3px #000'}
-                                }>About</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to ='/pages/resume' exact activeStyle={
-                                    {borderTop: 'solid 3px #000'} 
-                                }>Resume</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to ='/pages/contact' exact activeStyle={
-                                    {borderTop: 'solid 3px #000'} 
-                                }>Contact</NavLink>
-                            </li>
-                        </ul>
+            <div className="container">
+                <header>
+                    <section className="title">
+                        Joseph Jennings III
                     </section>
-                </Router>
-            </header>
+                        <section className="menu">
+                            <ul>
+                                <li>
+                                    <NavLink to ='/' exact activeStyle={
+                                        {borderTop: 'solid 3px #000'}
+                                    }>Projects</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to ='/pages/about' exact activeStyle={
+                                        {borderTop: 'solid 3px #000'}
+                                    }>About</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to ='/pages/resume' exact activeStyle={
+                                        {borderTop: 'solid 3px #000'} 
+                                    }>Resume</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to ='/pages/contact' exact activeStyle={
+                                        {borderTop: 'solid 3px #000'} 
+                                    }>Contact</NavLink>
+                                </li>
+                            </ul>
+                        </section>
+                </header>
+                <Switch>
+                            <Route exact path= '/pages/about' component={About} />
+                            <Route exact path= '/pages/contact' component={Contact} />
+                            <Route exact path= '/pages/resume' component={Resume} />
+                        </Switch>
+            </div>
         );
     }
 }
