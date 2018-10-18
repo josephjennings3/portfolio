61c94deb92be21dc9a90902548eeec47808e024b
Joseph Jennings III <jajenningsiii@gmail.com>
2018-10-18 12:50:07 -0700

    Added animations and applied to Intro. Also changed title.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import '../css/home.css';
+import {Animated} from "react-animated-css";
+
 
 class Home extends Component {
     render() {
       return (
         <div className="container-home">
-            <div className="home">
-                <div className="intro">
-                    Hello, I'm Joseph Jennings III
+            <Animated animationIn="fadeInDown" isVisible={true} className="animated">
+                <div className="home">
+                    <div className="intro">
+                        Hello, I'm Joseph Jennings III
+                    </div>
+                    <div className="intro-sub">
+                        I am a <span>Front-End Developer</span> based in San Diego training in Full Stack and interested UI/UX design.
+                    </div>
                 </div>
-                <div className="intro-sub">
-                    I am a <span>Front-End Developer</span> based in San Diego training in Full Stack and interested UI/UX design.
-                </div>
-            </div>
+            </Animated>
         </div>
       );
     }
